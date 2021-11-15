e358a8d3d5f41fce9b7c01694c8ecc6120ea747f
ArconRM <85395885+ArconRM@users.noreply.github.com>
2021-11-15 14:01:33 +0400

    Initial Commit

---
+++ WooordHunt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WooordHunt
+//
+//  Created by Artemiy Mirotvortsev on 15.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WooordHunt/WooordHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  WooordHuntApp.swift
+//  WooordHunt
+//
+//  Created by Artemiy Mirotvortsev on 15.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WooordHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
